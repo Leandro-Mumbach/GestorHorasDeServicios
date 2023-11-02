945f79e8692254d33224aa824dde601286e8b120
Leandro-Mumbach <lean21mumbach@gmail.com>
2023-11-02 04:57:58 -0300

    add logo and style frontend

--- RazorPageGHDS/Program.cs
+++ RazorPageGHDS/Program.cs
@@ -1,57 +1,7 @@
-using GestorHorasDeServicios.DataAccess;
-using GestorHorasDeServicios.Repository;
-using GestorHorasDeServicios.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
-
-
-// Add DTO
-builder.Services.AddAutoMapper(typeof(Program));
-// Add JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(o =>
-{
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = false,
-        ValidateIssuerSigningKey = true
-    };
-});
-builder.Services.AddAuthorization();
-// Add configuration from appsettings.json
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-//Conexion a base de datos
-builder.Services.AddDbContext<GestorDbContext>
-    (options => options.UseSqlServer("name=ConnectionStrings:Connection"));
-//Add registros servicios
-builder.Services.AddScoped<ITrabajosRepository, TrabajosRepository>();
-builder.Services.AddScoped<ITrabajosServices, TrabajosServices>();
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<IUsuarioServices, UsuarioServices>();
-builder.Services.AddScoped<IProyectosRepository, ProyectosRepository>();
-builder.Services.AddScoped<IProyectosServices, ProyectosServices>();
-builder.Services.AddScoped<IServiciosRepository, ServiciosRepository>();
-builder.Services.AddScoped<IServiciosServices, ServiciosServices>();
-
-
 
 
 var app = builder.Build();
